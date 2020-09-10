a517fe16a33a41a35b8b366e7de36ca869b3246c
subyster <arthur.amaral1@gmail.com>
2020-09-10 15:09:36 -0300

    variável env ROOT

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,10 @@ const path = requre('path');
 module.exports = {
   client: 'sqlite3',
   connection: {
-    filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+    filename: path.resolve(__dirname, process.env.ROOT, 'database', 'database.sqlite')
   },
   migrations: {
-    directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+    directory: path.resolve(__dirname, process.env.ROOT, 'database', 'migrations')
   },
   useNullAsDefault: true,
 }
